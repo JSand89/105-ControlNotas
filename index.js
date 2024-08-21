014a7998bed76895080f36dde4a9cfea19ce3ee3
JSand89 <javier.esand@gmail.com>
2024-08-21 07:19:59 -0500

    base de datos conectada

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 const app = express()
 const port = 3005 
 
 app.set("port",port)
 
+mongoose.connect(process.env.MONGO_DB_URI)
+.then(()=> console.log("Conectado a la BD"))
+.catch((err)=>console.error(err.message))
+
 app.get("/",(req,res)=>{
     res.send("Probando el servidor")
 })
+
+
 
 app.listen(port,()=>{
     console.log(`Escuchando el ${port}`)
